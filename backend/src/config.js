b293aa2e63e321f01226a8ea89a590f5e0110d64
Neil_Hassett_ALIEN_NFT <socailmedia@circuitfactory.ae>
2022-08-25 22:37:28 +0400

    UPDATE

--- backend/src/config.js
+++ backend/src/config.js
@@ -218,7 +218,7 @@ const PUBLIC_MINT_START_DATE = "2022-08-24T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-08-23T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1100; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 700; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x58ABD652A8260ddbDBbeca8D56870d7D4DD02906"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
