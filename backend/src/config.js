44177024496df36fa658cf61796b72e31cb953eb
Neil_Hassett_ALIEN_NFT <socailmedia@circuitfactory.ae>
2022-09-16 19:21:07 +0400

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -217,7 +217,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-10-05T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-09-30T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-10-02T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 700; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x58ABD652A8260ddbDBbeca8D56870d7D4DD02906"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
